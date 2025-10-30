22d4645e60f5b7fcb1f31297b6665836f0e3c53a
benjawl-alt <benj.vasquezc@duocuc.cl>
2025-10-30 18:26:23 -0300

    Update Inicio.jsx

--- my-app/src/pages/Inicio.jsx
+++ my-app/src/pages/Inicio.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-scroll";
-import "../assets/styles.css"; // Asegúrate de importar tu CSS aquí
+import "../assets/styles.css"; 
 
 export default function Inicio() {
   return (
